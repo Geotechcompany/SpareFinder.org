a9c348cc6f4066553696ccb925b9f874287e2e64
Geotechcompany <geoffreyaudia9@gmail.com>
2025-07-10 23:28:30 +0300

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: process.env.VITE_API_URL || 'http://localhost:4000',
         changeOrigin: true,
         secure: false
       }
